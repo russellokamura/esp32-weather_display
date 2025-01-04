c454c50e4c511071af437f8df001c840a0ff7529
russellokamura <russell.okamura@gmail.com>
2025-01-04 11:20:15 -0800

    added gitignore andled toggle working

---
+++ weather_display/main/inc/bme280.h
@@ -0,0 +1,19 @@
+#ifndef BME280_H
+#define BME280_H
+
+#define BME_ID_REG             (0xD0)
+#define BME_RESET_REG          (0xE0)
+#define BME_CRL_HUM_REG        (0xF2)
+#define BME_STATUS_REG         (0xF3)
+#define BME_CRTL_MEAS_REG      (0xF4)
+#define BME_CONFIG_REG         (0xF5)
+#define BME_PRESS_MSB_REG      (0xF7)
+#define BME_PRESS_LSB_REG      (0xF8)
+#define BME_PRESS_XLSB_REG     (0xF9)
+#define BME_TEMP_MSB_REG       (0xFA)
+#define BME_TEMP_LSB_REG       (0xFB)
+#define BME_TEMP_XLSB_REG      (0xFC)
+#define BME_HUM_MSB_REG        (0xFD)
+#define BME_HUM_LSB_REG        (0xFE)
+
+#endif // BME280_H
--- weather_display/main/weather_display.c
+++ weather_display/main/weather_display.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdint.h>
+#include "freertos/freeRTOS.h"
+#include "freertos/task.h"
+
+#include "inc/esp32_registers.h"
+
+#define GPIO18 18
+#define DELAY_MS 500
 
 void app_main(void)
 {
+   volatile uint32_t* gpio_out_w1ts_reg = (volatile uint32_t*) GPIO_OUT_W1TS_REG;
+   volatile uint32_t* gpio_out_w1tc_reg = (volatile uint32_t*) GPIO_OUT_W1TC_REG;
+   volatile uint32_t* gpio_enable_w1ts_reg = (volatile uint32_t*) GPIO_ENABLE_W1TS_REG;
+   volatile uint32_t* gpio_pin18_reg = (volatile uint32_t*) GPIO_PIN18_REG;
+   volatile uint32_t* gpio18_in_func_reg = (volatile uint32_t*) GPIO_FUNC18_IN_SEL_CFG_REG;
+   volatile uint32_t* gpio18_out_func_reg = (volatile uint32_t*) GPIO_FUNC18_OUT_SEL_CFG_REG;
+   volatile uint32_t* gpio18_io_mux_reg = (volatile uint32_t*) IO_MUX_GPIO18_REG;
 
+   *gpio_enable_w1ts_reg = (1 << GPIO18);
+   *gpio18_out_func_reg = (1 << GPIO_FUNC_OUT_SEL_CFG_OEN_SEL_Pos) | (256 << GPIO_FUNC_OUT_SEL_CFG_OUT_SEL_Pos);
+   *gpio_pin18_reg = 0;
+   *gpio18_in_func_reg = 0;
+   *gpio18_io_mux_reg = (2 << IO_MUX_FUN_DRV_Pos) | (2 << IO_MUX_MCU_DRV_Pos) | (2 << IO_MUX_MCU_SEL_Pos) | (1 << IO_MUX_MCU_OE_Pos);
+   
+
+   while(1) {
+      printf("hello world!\r\n");
+      *gpio_out_w1ts_reg = (1 << GPIO18);
+      vTaskDelay(pdMS_TO_TICKS(DELAY_MS));
+      *gpio_out_w1tc_reg = (1 << GPIO18);
+      vTaskDelay(pdMS_TO_TICKS(DELAY_MS));
+   }
 }
